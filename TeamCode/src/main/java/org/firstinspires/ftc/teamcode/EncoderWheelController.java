e899240084d2ccbaf7f181b2ba0b3fa0bb495aa3
Yu266426 <yu266426@gmail.com>
2024-01-22 19:34:05 -0800

    Update repo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderWheelController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderWheelController.java
@@ -4,13 +4,19 @@ package org.firstinspires.ftc.teamcode;
 //
 //}
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class EncoderWheelController {
     private int frontLeftWheelPos, frontRightWheelPos, backLeftWheelPos, backRightWheelPos;
 
     EncoderWheelController() {
         // Reset Wheel Encoders
-        Hardware.frontLeftDriveMotor.setMode();
+        Hardware.frontLeftDriveMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Hardware.frontRightDriveMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Hardware.backLeftDriveMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Hardware.backLeftDriveMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
+        
     }
 
 
